cb4a153d8ccb93c6f082804d6d1d51a9725cffa9
Khushi Chitalia <kjchitalia@gmail.com>
2023-10-25 16:29:50 -0400

    second commit

--- Lab6.py
+++ Lab6.py
@@ -1,5 +1,3 @@
-# Gareth Bacchus Lab 6
-
 def encoder(passcode):
     result = ''
     for digit in passcode:
@@ -7,6 +5,12 @@ def encoder(passcode):
         result += new_digit  # Obtains the new digit
     return result  # Output
 
+def decoder(passcode):
+    result = ''
+    for digit in passcode:
+        new_digit = str((int(digit) - 3) % 10)
+        result += new_digit
+    return result
 
 def main():
     while True:
@@ -17,9 +21,10 @@ def main():
         if choice == "1":
             value = input("Please enter your password to encode: ")
             print("Your password has been encoded and stored!")
+            encoded_passcode = encoder(value)
         elif choice == "2":
-            print("The encoded password is", encoder(value))
-            print("and the original password is", value)
+            decoded_password = decoder(encoded_passcode)
+            print("The encoded password is", encoded_passcode, "and the original password is", decoded_password)
         elif choice == "3":
             break
         else:
